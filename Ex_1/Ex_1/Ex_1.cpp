5f9c99d1d803d729b66ec01f1e444e5d57c3d479
Warakonn <52008268+Warakonn@users.noreply.github.com>
2019-06-26 23:26:49 +0700

    Add files via upload

---
+++ Ex_1/Ex_1/Ex_1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	int money;
+
+	cout <<"****Exchange Rate****"<<"\n";
+	cout <<"Enter money : ";
+	cin >> money ;
+	cout << money << " "<< "Baths is "<< " " << money/30.75 <<" "<<"dollars"<<"\n";
+	cout << money << " "<< "Baths is "<< " " << money/35.03 <<" "<<"Euro"<<"\n";
+	return(0);
+}
